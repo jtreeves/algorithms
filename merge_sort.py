746049fc598551aa5c9111bb3743c890acf70f34
jtreeves <jackson.t.reeves@gmail.com>
2021-01-12 10:21:23 -0500

    [merge_sort] Delete unnecessary print statements

--- merge_sort.py
+++ merge_sort.py
@@ -2,10 +2,6 @@ def merge(set1, set2):
     output = []
     combined_start_lengths = len(set1) + len(set2)
     while len(output) < combined_start_lengths:
-        # print(f'output: {output}')
-        # print(f'set1: {set1}')
-        # print(f'set2: {set2}')
-        # print('----------')
         if len(set1) == 0:
             output += set2
             set2 = []
@@ -20,15 +16,6 @@ def merge(set1, set2):
             set2 = set2[1:]
     return output
 
-# print(merge([2], [4])) # => [2, 4]
-# print(merge([4], [2])) # => [2, 4]
-# print(merge([4, 5, 11], [2, 9])) # => [2, 4, 5, 9, 11]
-# print(merge([4, 5, 11], [2, 5, 9])) # => [2, 4, 5, 5, 9, 11]
-# print(merge([4, 5, 11, 36, 37], [2, 5, 9, 11, 13])) # => [2, 4, 5, 5, 9, 11, 11, 13, 36, 37]
-
-# ALTERNATIVE WHILE LOOP
-# while len(set1) > 0 or len(set2) > 0
-
 def split(input):
     midpoint = len(input) // 2
     set1 = input[:midpoint]
@@ -39,6 +26,4 @@ def split(input):
     split_set2 = split(set2)
     return merge(split_set1, split_set2)
 
-print(split([1, 2, 3, 4]))
-print(split([1, 2, 3, 4, 5]))
 print(split([2, 3, 1, 7, 4, 5]))
