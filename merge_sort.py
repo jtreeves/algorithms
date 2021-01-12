b115bf9fe996b2f4b55f46af819e26123a170752
jtreeves <jackson.t.reeves@gmail.com>
2021-01-12 09:59:28 -0500

    [merge_sort] Build out split helper function

--- merge_sort.py
+++ merge_sort.py
@@ -1,11 +1,21 @@
+def split(input):
+    midpoint = len(input) // 2
+    set1 = input[:midpoint]
+    set2 = input[midpoint:]
+    # return set1, set2
+    
+
+print(split([1, 2, 3, 4]))
+print(split([1, 2, 3, 4, 5]))
+
 def merge(set1, set2):
     output = []
     combined_start_lengths = len(set1) + len(set2)
     while len(output) < combined_start_lengths:
-        print(f'output: {output}')
-        print(f'set1: {set1}')
-        print(f'set2: {set2}')
-        print('----------')
+        # print(f'output: {output}')
+        # print(f'set1: {set1}')
+        # print(f'set2: {set2}')
+        # print('----------')
         if len(set1) == 0:
             output += set2
             set2 = []
@@ -20,11 +30,11 @@ def merge(set1, set2):
             set2 = set2[1:]
     return output
 
-print(merge([2], [4])) # => [2, 4]
-print(merge([4], [2])) # => [2, 4]
-print(merge([4, 5, 11], [2, 9])) # => [2, 4, 5, 9, 11]
-print(merge([4, 5, 11], [2, 5, 9])) # => [2, 4, 5, 5, 9, 11]
-print(merge([4, 5, 11, 36, 37], [2, 5, 9, 11, 13])) # => [2, 4, 5, 5, 9, 11, 11, 13, 36, 37]
+# print(merge([2], [4])) # => [2, 4]
+# print(merge([4], [2])) # => [2, 4]
+# print(merge([4, 5, 11], [2, 9])) # => [2, 4, 5, 9, 11]
+# print(merge([4, 5, 11], [2, 5, 9])) # => [2, 4, 5, 5, 9, 11]
+# print(merge([4, 5, 11, 36, 37], [2, 5, 9, 11, 13])) # => [2, 4, 5, 5, 9, 11, 11, 13, 36, 37]
 
 # ALTERNATIVE WHILE LOOP
 # while len(set1) > 0 or len(set2) > 0
